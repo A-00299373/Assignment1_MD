080c167492a9f9a899a90705ad582ad7e108ae06
Aanya2 <manav01patel@gmail.com>
2024-03-26 18:39:37 -0400

    2nd commit

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -8,8 +8,8 @@ var plainDecorator = new Plain_decoratorClass(component);
 var uppercaseDecorator = new Uppercase_decoratorClass(component);
 var colorDecorator = new Color_decoratorClass(component);
 
-Console.WriteLine("Plain Decorator: " + plainDecorator.GetText());
-Console.WriteLine("Uppercase Decorator: " + uppercaseDecorator.GetText());
-Console.WriteLine("Color Decorator: " + colorDecorator.GetText());
+Console.WriteLine("First Decorator(Prefix): " + plainDecorator.GetText());
+Console.WriteLine("Second Decorator(Uppercase): " + uppercaseDecorator.GetText());
+Console.WriteLine("Third Decorator(Color): " + colorDecorator.GetText());
 
 
